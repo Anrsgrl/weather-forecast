812f63058372f0b64e5e76e04eb64fab80a563f2
Anrsgrl <esgerlianar003@gmail.com>
2023-03-07 16:59:38 +0300

    weather modal is finished / responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.scss';
 import SearchField from './components/SearchField/SearchField';
 import bg from "./assets/images/weatherrr.jpg"
+import WeatherModal from './components/WeatherModal/WeatherModal';
 
 function App() {
   return (
     <div className="App">
-      <img src={bg} alt="" />
+      <img className='background' src={bg} alt="background" />
       <SearchField />
+      <WeatherModal />
     </div>
   );
 }
--- src/components/SearchField/SearchField.jsx
+++ src/components/SearchField/SearchField.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./searchField.scss";
 import { BiSearchAlt } from "react-icons/bi"
+import axios from 'axios';
 
 const SearchField = () => {
+    const [data, setData] = useState([]);
+    const [city, setCity] = useState("");
+    console.log(data)
+    const findCity = async () => {
+        const apiKey = "51a961e03c49876e078e90d6f9aa762a";
+        const baseURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=eng&appid=${apiKey}&units=metric`
+
+        await axios(baseURL).then(res => setData(res.data));
+    }
     return (
         <div className="search-field">
             <h5>Check the weather by the city </h5>
-            <div class="inputs">
-                <input type="text" class="form-control" placeholder="Search City" />
-                <button className='search'><BiSearchAlt /></button>
+            <div className="inputs">
+                <input onChange={(e) => setCity(e.target.value)} type="text" className="form-control" placeholder="Search City" />
+                <button onClick={() => { findCity() }} className='search'><BiSearchAlt /></button>
             </div>
         </div>
     )
